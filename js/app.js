8744c1906064cca34765252831a6beafb7508bd2
MdTasib <ohidulalam442@gmail.com>
2022-03-11 11:52:11 +0600

    issue 06 - liked posts displayed

--- js/app.js
+++ js/app.js
@@ -150,8 +150,19 @@ const showPosts = posts => {
 	});
 };
 
+// const displayLikedPosts = () => {
+// 	const likedPosts = getLikedPosts();
+// 	likedPosts.forEach(post => {
+// 		const div = createPost(post);
+// 		document.getElementById("liked").appendChild(div);
+// 	});
+// };
+
 const displayLikedPosts = () => {
 	const likedPosts = getLikedPosts();
+	const likedPostsContainer = document.getElementById("liked");
+	likedPostsContainer.innerHTML = "";
+
 	likedPosts.forEach(post => {
 		const div = createPost(post);
 		document.getElementById("liked").appendChild(div);
